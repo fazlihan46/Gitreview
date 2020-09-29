0cc098aa022c63188c92ad3bf0ba25ec27d0114b
fazlikuzhan <fzlkzhn46@gmail.com>
2020-09-29 12:25:49 -0700

    selamalar

--- src/test/java/Ders1.java
+++ src/test/java/Ders1.java
@@ -4,5 +4,6 @@ public class Ders1 {
     public static void main(String[] args) {
         System.out.println("saat 18:46");
         System.out.println("saat 18:46");
+        System.out.println("fazlidan selamlar");
     }
 }
